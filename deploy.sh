1759eb5dd6d18012a527fe29d18a3fee16b79745
Tochi-Nwachukwu <tochinwachukwu33@gmail.com>
2025-05-22 14:05:31 +0100

    [Updated the deploy script]

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ echo "deleting old app"
 sudo rm -rf /var/www/
 
 echo "creating app folder"
-sudo mkdir -p /var/www/langchain-app 
+sudo mkdir -p /var/www/python-app 
 
 echo "moving files to app folder"
-sudo mv  * /var/www/langchain-app
+sudo mv  * /var/www/python-app
 
 # Navigate to the app directory
-cd /var/www/langchain-app/
+cd /var/www/python-app/
 sudo mv env .env
 
 sudo apt-get update
@@ -29,21 +29,21 @@ if ! command -v nginx > /dev/null; then
 fi
 
 # Configure Nginx to act as a reverse proxy if not already configured
-if [ ! -f /etc/nginx/sites-available/myapp ]; then
+if [ ! -f /etc/nginx/sites-available/pythonapp ]; then
     sudo rm -f /etc/nginx/sites-enabled/default
-    sudo bash -c 'cat > /etc/nginx/sites-available/myapp <<EOF
+    sudo bash -c 'cat > /etc/nginx/sites-available/pythonapp <<EOF
 server {
     listen 80;
     server_name _;
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:/var/www/langchain-app/myapp.sock;
+        proxy_pass http://unix:/var/www/python-app/pythonapp.sock;
     }
 }
 EOF'
 
-    sudo ln -s /etc/nginx/sites-available/myapp /etc/nginx/sites-enabled
+    sudo ln -s /etc/nginx/sites-available/pythonapp /etc/nginx/sites-enabled
     sudo systemctl restart nginx
 else
     echo "Nginx reverse proxy configuration already exists."
@@ -51,11 +51,11 @@ fi
 
 # Stop any existing Gunicorn process
 sudo pkill gunicorn
-sudo rm -rf myapp.sock
+sudo rm -rf pythonapp.sock
 
 # # Start Gunicorn with the Flask application
 # # Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
 # # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
 echo "starting gunicorn"
-sudo gunicorn --workers 3 --bind unix:myapp.sock  server:app --user www-data --group www-data --daemon
+sudo gunicorn --workers 3 --bind unix:pythonapp.sock  server:app --user www-data --group www-data --daemon
 echo "started gunicorn 🚀"
